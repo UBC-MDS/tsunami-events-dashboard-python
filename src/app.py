d62d275d618baaf811ac216c45a1138d8a806ff5
Rowansiv <90011761+Rowansiv@users.noreply.github.com>
2022-03-01 14:37:14 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from dash import Dash, html
 
-app = Dash()
+app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
 
 app.layout = html.Div("Hello world!")
