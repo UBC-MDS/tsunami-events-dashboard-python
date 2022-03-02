77f8b58366f5305bbfb6aac227d98539eeabff1e
Rowansiv <90011761+Rowansiv@users.noreply.github.com>
2022-03-02 13:21:28 -0800

    Update dropdown.py

--- src/dropdown.py
+++ src/dropdown.py
@@ -4,48 +4,32 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import altair as alt
 
-tsunami_df = pd.read_csv("data/processed/tsunami-events.csv")
+# tsunami_df = pd.read_csv("data/processed/tsunami-events.csv")
+
+cars = data.cars()
+
+def plot_altair(xmax):
+    chart = alt.Chart(cars[cars['Horsepower'] < xmax]).mark_bar().encode(
+        x='Weight_in_lbs:Q',
+        y='Horsepower')
+    return chart.to_html()
 
 app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
-server = app.server
-
-table_cols = ["Country", "Location Name", "Year", "Month", "Tsunami Intensity", "Earthquake Magnitude"]
 
 app.layout = html.Div([
-              dash_table.DataTable(
-                id = "tsunami_table",
-                columns=[
-                    {"name": i, "id": i, "deletable": False, "selectable": False} for i in table_cols
-                ],
-                data = tsunami_df.to_dict('records'),
-                editable=True,
-                sort_action="native",
-                sort_mode="multi",
-                column_selectable=False,
-                row_selectable=False,
-                row_deletable=True,
-                filter_action="native",
-                page_action="native",
-                page_current= 0,
-                page_size= 10),
-html.Button('Add Row', id='editing-rows-button', n_clicks=0)])
-
+        html.Iframe(
+            id='bar',
+            srcDoc=plot_altair(xmax=0),
+            style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+        dcc.Slider(id='xslider', min=0, max=240)])
+        
 @app.callback(
-    Output('tsunami_table', 'data'),
-    Input('editing-rows-button', 'n_clicks'))
-def add_row(n_clicks, rows, columns):
-    if n_clicks > 0:
-        rows.append({c['id']: '' for c in columns})
-    return rows
+    Output('bar', 'srcDoc'),
+    Input('xslider', 'value'))
+def update_output(xmax):
+    return plot_altair(xmax)
 
-# @app.callback(
-#     Output('tsunami_table', 'data'),
-#     Input('top_dropdown', 'value'),
-# #     Input('year_slider', 'value')
-# )
-# def update_table():
-#   tsunami = tsunami_df
-#   tsunami = tsunami.sort_values(by = ['tsunami_intensity'])
-#   return tsunami.to_dict('records')
-  
+if __name__ == '__main__':
+    app.run_server(debug=True)
