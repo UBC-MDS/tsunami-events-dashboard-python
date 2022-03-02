af92889499fd6172d0531cc0c617309ffa27303c
Rowansiv <90011761+Rowansiv@users.noreply.github.com>
2022-03-02 13:41:21 -0800

    Update dropdown.py

--- src/dropdown.py
+++ src/dropdown.py
@@ -6,30 +6,55 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import altair as alt
 
-# tsunami_df = pd.read_csv("data/processed/tsunami-events.csv")
+tsunami_df = pd.read_csv("data/processed/tsunami-events.csv")
 
-cars = data.cars()
-
-def plot_altair(xmax):
-    chart = alt.Chart(cars[cars['Horsepower'] < xmax]).mark_bar().encode(
-        x='Weight_in_lbs:Q',
-        y='Horsepower')
-    return chart.to_html()
+bar = html.Iframe(
+    id="bar",
+    style={
+        "border-width": "0",
+        "width": "100%",
+        "height": "400px",
+    })
 
 app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
 app.layout = html.Div([
         html.Iframe(
-            id='bar',
-            srcDoc=plot_altair(xmax=0),
-            style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-        dcc.Slider(id='xslider', min=0, max=240)])
+                id="bar",
+                style={
+                    "border-width": "0",
+                    "width": "100%",
+                    "height": "400px"})
         
 @app.callback(
-    Output('bar', 'srcDoc'),
-    Input('xslider', 'value'))
-def update_output(xmax):
-    return plot_altair(xmax)
+    Output("bar", "srcDoc"),
+    Input("year_slider", "value"))
+def plot_bar(year_slider):
+    tsunami = tsunami_df
+    tsunami = tsunami.sort_values(by = ['tsunami_intensity'])
+    tsunami = tsunami[:11]
+    chart = (
+        alt.Chart(
+            tsunami,
+            title="Strongest Tsunamis",
+        )
+        .mark_bar()
+        .encode(
+            y=alt.Y("country", sort="-x", title="Country"),
+            x=alt.X(tsunami_intensity, title="Tsunami Intensity"),
+            color=alt.Color(
+                "country",
+                sort=alt.EncodingSortField("country", order="descending"),
+                title="Country",
+            ),
+            tooltip=("name:O", "tsunami_intensity:Q"),
+        )
+        .configure_axis(labelFontSize=12, titleFontSize=14)
+        .configure_title(fontSize=15)
+        .configure_legend(labelFontSize=12)
+        .properties(width=400, height=300)
+    )
+    return chart.to_html()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
