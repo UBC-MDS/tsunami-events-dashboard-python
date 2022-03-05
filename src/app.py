1ee60dcfa00ba8b8aae5271d2c40d5e6e899d5d7
jacqann <jacqueline.ann.chong@gmail.com>
2022-03-05 08:00:12 -0800

    Removed comments, changed dash title, removed spacing after =

--- src/app.py
+++ src/app.py
@@ -7,21 +7,19 @@ from components.dropdown import create_bar_plot
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-# Read in data
-tsunami_df = pd.read_csv('data/processed/tsunami-events.csv')
+tsunami_df=pd.read_csv('data/processed/tsunami-events.csv')
 
-years = tsunami_df['year'].dropna().unique() # need to add start and end year
-countries = tsunami_df['country'].dropna().unique()
-country_list = sorted(list(countries)) # countries are listed alphabetically
+years=tsunami_df['year'].dropna().unique()
+countries=tsunami_df['country'].dropna().unique()
+country_list=sorted(list(countries))
 
-app = dash.Dash(
-    __name__, title = "Tsunami History", external_stylesheets=[dbc.themes.QUARTZ]
+app=dash.Dash(
+    __name__, title="🌊 Tsunami Events 🌊", external_stylesheets=[dbc.themes.QUARTZ]
 )
 
-server = app.server
+server=app.server
 
-# Creating style for both sidebar and plots
-SIDEBAR_STYLE = {
+SIDEBAR_STYLE={
     "position": "fixed",
     "top": '62px',
     "left": 0,
@@ -32,38 +30,31 @@ SIDEBAR_STYLE = {
 
 }
 
-CONTENT_STYLE = {
+CONTENT_STYLE={
     "margin-left": "20rem",
     "z-index": -1,
 }
-# Structure of app, including selection criteria components
 
-# Formatting NavBar
-navbar = dbc.Navbar(
+navbar=dbc.Navbar(
     dbc.Container(
         [
             html.A(
-                # Use row and col to control vertical alignment of logo / brand
                 dbc.Row(
                     [
-                        # dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
                         dbc.Col(dbc.NavbarBrand("Tsunami Events Dashboard")),
                     ],
                     align="center",
                     className="g-0",
                 ),
-                # href="https://plotly.com",
-                # style={"textDecoration": "none"},
             ),
             dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
             dbc.Collapse(
-                # search_bar,
                 id="navbar-collapse",
                 is_open=False,
                 navbar=True,
             ),
         ],
-        style = {
+        style={
             'margin-left': '1.2rem',
             'font-weight': "600"
         }
@@ -72,97 +63,97 @@ navbar = dbc.Navbar(
     dark=True,
 )
 
-world_plot_card = dbc.Card(
+world_plot_card=dbc.Card(
     dbc.CardBody(
-        [html.H6('Total Tsunami Hits by Country with Origin Points', className = 'card-title',
-        style = {'margin-bottom': '0px'}),
+        [html.H6('Total Tsunami Hits by Country with Origin Points', className='card-title',
+        style={'margin-bottom': '0px'}),
         html.Iframe(
             id='map_plot',
             style={'border-width': '0', 'height': '390px', 'width': '100%'},
             srcDoc=create_map_plot(year_start=1800, year_end=2022, countries=[]))
-        ], style = {'padding': '15px', 'padding-bottom': '0px'}
+        ], style={'padding': '15px', 'padding-bottom': '0px'}
     ),
-    style = {'padding': 0}
+    style={'padding': 0}
 )
 
-scatter_plot_card = dbc.Card(
+scatter_plot_card=dbc.Card(
     dbc.CardBody(
-        [html.H6('Top 10 Countries\' Total Deaths by Earthquake Magnitude', className = 'card-title',
-        style = {'margin-bottom': '0px'}),
+        [html.H6('Top 10 Countries\' Total Deaths by Earthquake Magnitude', className='card-title',
+        style={'margin-bottom': '0px'}),
         html.Iframe(
-            id = 'scatter_plot',
+            id='scatter_plot',
             style={'border-width': '0', 'height': '250px','width': '100%'},
             srcDoc=create_scatter_plot(year_start=1800, year_end=2022, countries=[]) 
-        )], style = {'padding': '15px', 'padding-bottom': '0px'}
+        )], style={'padding': '15px', 'padding-bottom': '0px'}
     ),
-    style = {'padding': 0}
+    style={'padding': 0}
 )
 
-bar_chart_card = dbc.Card(
+bar_chart_card=dbc.Card(
     dbc.CardBody(
-        [html.H6('10 Most Intense Tsunamis by Country', className = 'card-title',
-        style = {'margin-bottom': '0px'}),
+        [html.H6('10 Most Intense Tsunamis by Country', className='card-title',
+        style={'margin-bottom': '0px'}),
         html.Iframe(
-            id = 'bar_plot',
+            id='bar_plot',
             style={'border-width': '0', 'height': '250px','width': '100%'},
             srcDoc=create_bar_plot(year_start=1800, year_end=2022)
-        )], style = {'padding': '15px', 'padding-bottom': '0px'}
+        )], style={'padding': '15px', 'padding-bottom': '0px'}
     ),
-    style = {'padding': 0}
+    style={'padding': 0}
 )
 
-app.layout = dbc.Container([
+app.layout=dbc.Container([
     navbar,
     dbc.Row([
         dbc.Col([
-            html.H5('Years and Countries Selection', className = 'form-label'),
+            html.H5('Years and Countries Selection', className='form-label'),
             html.Hr(),
-            html.H6('Years of Interest (1800 - 2022)', className = 'form-label'),
+            html.H6('Years of Interest (1800 - 2022)', className='form-label'),
             dcc.RangeSlider(
-                min = 1800, 
-                max = 2022,
-                value = [tsunami_df['year'].min(), tsunami_df['year'].max()],
-                marks = None, 
+                min=1800, 
+                max=2022,
+                value=[tsunami_df['year'].min(), tsunami_df['year'].max()],
+                marks=None, 
                 id='year_slider',
-                allowCross = False, # Prevent handles from crossing each other
-                tooltip = {'placement': 'bottom',
+                allowCross=False, 
+                tooltip={'placement': 'bottom',
                             'always_visible': True}),
             html.Br(),
             html.Br(),
-            html.H6('Countries of Interest', className = 'form-label'),
+            html.H6('Countries of Interest', className='form-label'),
             dcc.Dropdown(
                 id='country_select',
-                multi = True,
-                value = [],
-                options = [{'label': country, 'value': country} for country in country_list],
-                className = 'text-dark'),
+                multi=True,
+                value=[],
+                options=[{'label': country, 'value': country} for country in country_list],
+                className='text-dark'),
             html.Hr(),
             html.P(
                 "A data visualisation app that allows viewers to observe the number and intensity of tsunamis based on years and countries",
-                className = 'form-label'
+                className='form-label'
             )
         ],
-        style = SIDEBAR_STYLE,
-        className = "btn btn-secondary"
+        style=SIDEBAR_STYLE,
+        className="btn btn-secondary"
         ),
         dbc.Col([
             dbc.Row([
                 world_plot_card
-            ], style = {'margin': 'auto', 'width': '820px', 'padding':'0px'}),
+            ], style={'margin': 'auto', 'width': '820px', 'padding':'0px'}),
             html.Br(),
             dbc.Row([
                 dbc.Col([
                     scatter_plot_card
-                ], style = {'margin': 'auto', 'width': '400px'}),
+                ], style={'margin': 'auto', 'width': '400px'}),
                 dbc.Col([
                     bar_chart_card
-                ], style = {'margin': 'auto', 'width': '400px'})
-            ],style = {'margin': 'auto', 'width': '1020px'})
+                ], style={'margin': 'auto', 'width': '400px'})
+            ],style={'margin': 'auto', 'width': '1020px'})
         ],
-        style = CONTENT_STYLE)
+        style=CONTENT_STYLE)
     ])
-], fluid = True,
-    style = {
+], fluid=True,
+    style={
         'backgroundColor': 'black',
         'padding': '0px',
         'height': '100vh',
@@ -195,6 +186,7 @@ def update_scatter_plot(value, value_country):
 def update_bar_plot(value):
     return create_bar_plot(value[0], value[1])
 
+# App callback for navbar
 @app.callback(
     Output("navbar-collapse", "is_open"),
     [Input("navbar-toggler", "n_clicks")],
