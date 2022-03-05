c8a2cd7c16676c6e5505d316eda95c43a921e316
Rowansiv <90011761+Rowansiv@users.noreply.github.com>
2022-03-04 20:50:59 -0800

    Update dropdown.py
    Creating new column 'combine' for bar chart text

--- src/components/dropdown.py
+++ src/components/dropdown.py
@@ -9,6 +9,7 @@ def preprocess(year_start, year_end):
     df = pd.read_csv(PROCESSED_DATA_PATH)
     df['tsunami_instance'] = range(1, len(df) + 1)
     df['tsunami_instance'] = df.index
+    df['combine'] = df['country'].astype(str) + ', ' + df['year'].astype(str)
     df = df.query(f"{year_start} <= year <= {year_end}")
     df = df.sort_values(by=['tsunami_intensity'], ascending = False)
     df = df[0:15]
@@ -23,7 +24,7 @@ def create_bar_plot(year_start, year_end):
         tooltip=("country:O", "location_name:O", "tsunami_intensity:Q", "earthquake_magnitude:Q", "year:Q", "month:O"))
     
     text = chart.mark_text(align="left", baseline="middle", dx = 3).encode(
-        text= 'country:O' + ', ' + 'year:Q')
+        text= 'combine:O')
     
     plot = chart + text
     return plot.to_html()
