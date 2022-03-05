be1b300d5b3dc341e55860e0077148ac11abb5c5
Rowansiv <90011761+Rowansiv@users.noreply.github.com>
2022-03-04 19:43:33 -0800

    Update dropdown.py

--- src/components/dropdown.py
+++ src/components/dropdown.py
@@ -5,19 +5,19 @@ import pandas as pd
 
 PROCESSED_DATA_PATH = "data/processed/tsunami-events.csv"
 
-def preprocess(value):
+def preprocess(year_start, year_end):
     df = pd.read_csv(PROCESSED_DATA_PATH)
     df['tsunami_instance'] = range(1, len(df) + 1)
     df['tsunami_instance'] = df.index
-    df_daterange = df.loc[(df['year'] >= value[0]), & df['year'] <= value[1])]
+    df_daterange = df.query(f"{year_start} <= year <= {year_end}")
     df_daterange = df_daterange.sort_values(by=['tsunami_intensity'], ascending = False)
     df_daterange = df_daterange[0:15]
     return df_daterange
 
-def create_bar_plot(value):
-    tsunami_dataframe = preprocess(value)
+def create_bar_plot(year_start, year_end):
+    tsunami_dataframe = preprocess(year_start, year_end)
     chart = alt.Chart(tsunami_dataframe).mark_bar().encode(
-        x=alt.X('tsunami_intensity:Q', title = 'Tsunami Intensity', scale=alt.Scale(domain=(0, 10))),
+        x=alt.X('tsunami_intensity:Q', title = 'Tsunami Intensity', scale=alt.Scale(domain=(0, 12))),
         y=alt.Y('tsunami_instance:N', sort = '-x', title = 'Country', axis = alt.Axis(labelExpr="datum.country")),
         tooltip=("country:O", "location_name:O", "tsunami_intensity:Q", "earthquake_magnitude:Q", "year:Q", "month:O"))
     
