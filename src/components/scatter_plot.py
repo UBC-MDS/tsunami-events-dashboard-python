e2b609e38ac5dceffc968f0305f9222b606d8c9f
Vadim Taskaev <vadim.taskaev@outlook.com>
2022-03-04 23:54:14 -0800

    Added docstrings and removed vertical gridlines in plot script

--- src/components/scatter_plot.py
+++ src/components/scatter_plot.py
@@ -2,43 +2,112 @@ import altair as alt
 from dash import Dash, dcc, html, Input, Output
 import pandas as pd
 
-PROCESSED_DATA_PATH = "data/processed/tsunami-events.csv"
+PROCESSED_DATA_PATH = 'data/processed/tsunami-events.csv'
+
+tsunami_df = pd.read_csv('data/processed/tsunami-events.csv')
+
+years = tsunami_df['year'].dropna().unique()
+countries = tsunami_df['country'].dropna().unique()
+country_list = sorted(list(countries))
 
 
-def get_data(ymin=1500, ymax=2020, value_ctry=None):
-    input = PROCESSED_DATA_PATH
-    df = pd.read_csv(input,
-                     parse_dates=True)[["year", "country", "total_deaths", "earthquake_magnitude"]]
-    df = df[df['total_deaths'].notna()]
-    df['mercalli_intensity'] = pd.cut(df['earthquake_magnitude'],
-                                      bins=[1, 2, 4, 5, 6, 7, 10],
-                                      labels=["I", "II-III", "IV–V", "VI–VII", "VII–IX", "VIII or higher"]).astype(str)
-    if value_ctry == None:
-        return df.loc[(df['year'] > ymin) & (df['year'] < ymax)]
-    else:
-        return df.loc[(df['year'] > ymin) &
-                      (df['year'] < ymax) &
-                      (df['country'].isin(value_ctry))
-                      ]
+def create_scatter_plot(year_start=1900, year_end=2022, countries=[]):
+    """
+    The function to create a scatter plot of earthquake intensity
+        versus total deaths recorded (on a log-scale) per tsunami event 
+        between the year_start and year_end for countries specified.
 
+    Parameters
+    ----------
+    year_start : int
+        the lower bound of the range of years selected by user
+    year_end : int
+        the upper bound of the range of years selected by user
+    countries : list
+        the selection of countries selected by user
 
-def create_scatter_plot(ymin, ymax, value_ctry):
-    chart = alt.Chart(get_data(ymin, ymax, value_ctry)).mark_point(opacity=0.5).encode(
+    Returns
+    -------
+    scatter plot object
+        scatter plot chart of earthquake intensity versus total deaths 
+        recorded (on a log-scale)
+    """
+    chart = alt.Chart(get_data(year_start, year_end, countries)
+                      ).mark_point(opacity=0.65, size=20).encode(
         x=alt.X('earthquake_magnitude',
                 title='Earthquake Magnitude (Richter Scale)',
-                scale=alt.Scale(domain=(5.5, 10))),
+                scale=alt.Scale(domain=(5.5, 10)),
+                axis=alt.Axis(grid=False)),
         y=alt.Y('total_deaths',
                 title='Total Deaths (log-transformed), per Event',
-                scale=alt.Scale(type='log', base=10)),
+                scale=alt.Scale(type='log')),
         color='country',
         tooltip=['year', 'mercalli_intensity', 'country', 'total_deaths']
     ).interactive(
-    ).properties(
-        title='Scatter Plot of Earthquake Magnitude vs Total Deaths, per Deadly Tsunami Event, by Country'
-    ).configure_title(
-        fontSize=10,
-        font='Courier',
-        anchor='start',
-        color='black'
     )
     return chart.to_html()
+
+
+def get_data(year_start=1900, year_end=2022, countries=[]):
+    """
+    The function to return the processed dataframe of original data including   
+        a new column computing the Mercalli Intensity scale per tsunami event, 
+        subsetting for observations between year_start and year_end, for the 
+        countries specified. In addition, should more than 10 countries be 
+        specified by the user, the function only returns observations for 
+        the 10 countries whose individual earthquake observations recorded 
+        largest magnitude per the year range and countries specified. 
+
+    Parameters
+    ----------
+    year_start : int
+        the lower bound of the range of years selected by user
+    year_end : int
+        the upper bound of the range of years selected by user
+    countries : list
+        the selection of countries selected by user
+
+    Returns
+    -------
+    df:
+        a processed dataframe with additional columns and filtered
+        data, comprising no more than 10 countries
+    """
+    tsunami_events = pd.read_csv(PROCESSED_DATA_PATH)
+
+    if not (year_start and year_end and year_start <= year_end):
+        raise ValueError("Invalid value for year start and/or year end")
+
+    if not countries and countries != []:
+        raise ValueError("Invalid value for countries")
+
+    tsunami_events = tsunami_events.query(
+        f"{year_start} <= year <= {year_end}"
+    )
+    if countries == []:
+        countries = tsunami_events.sort_values(
+            by="earthquake_magnitude", ascending=False)[
+            "country"].drop_duplicates().tolist()[:5]
+
+    tsunami_events = tsunami_events[tsunami_events['total_deaths'].notna()]
+    tsunami_events['mercalli_intensity'] = pd.cut(
+        tsunami_events['earthquake_magnitude'],
+        bins=[1, 2, 4, 5, 6, 7, 10],
+        labels=["I", "II-III", "IV–V", "VI–VII", "VII–IX", "VIII or higher"]
+    ).astype(str)
+
+    if len(countries) > 10:
+        countries_top10 = [x for x in tsunami_events.sort_values(
+            by="earthquake_magnitude", ascending=False)[
+            "country"].drop_duplicates().tolist() if x in countries][:10]
+
+        return tsunami_events.loc[(tsunami_events['year'] > year_start) &
+                                  (tsunami_events['year'] < year_end) &
+                                  (tsunami_events['country'].isin(
+                                      countries)) &
+                                  (tsunami_events['country'].isin(
+                                      countries_top10))]
+    else:
+        return tsunami_events.loc[(tsunami_events['year'] > year_start) &
+                                  (tsunami_events['year'] < year_end) &
+                                  (tsunami_events['country'].isin(countries))]
