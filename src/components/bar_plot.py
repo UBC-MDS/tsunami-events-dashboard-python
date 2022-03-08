15f33e03d6cede898bc4978e8ef0e1139b25eb35
Rowansiv <90011761+Rowansiv@users.noreply.github.com>
2022-03-08 14:29:56 -0800

    Editing bar plot legend

--- src/components/bar_plot.py
+++ src/components/bar_plot.py
@@ -49,7 +49,7 @@ def create_bar_plot(year_start, year_end):
     chart = alt.Chart(df).mark_bar().encode(
         x=alt.X('tsunami_intensity:Q', title = 'Tsunami Intensity', scale=alt.Scale(domain=(0, 12))),
         y=alt.Y('tsunami_instance:N', sort = '-x', title = 'Country', axis = alt.Axis(labelExpr="datum.country")),
-        color=alt.Color('country:O'),
+        color=alt.Color('country:O', legend=alt.Legend(title="Countries (up to Top 10)")),
         tooltip=("country:O", "location_name:O", "tsunami_intensity:Q", "earthquake_magnitude:Q", "year:Q", "month:O")
         ).properties(width=250, height=180)
     
