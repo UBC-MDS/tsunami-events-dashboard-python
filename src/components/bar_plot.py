d50e930828a1a9927a1404891a297484d8fe7c1f
Rowan Sivanandam <rowan.sivanandam@gmail.com>
2022-03-08 15:37:02 -0800

    Resizing legend to remove scrolling

--- src/components/bar_plot.py
+++ src/components/bar_plot.py
@@ -49,15 +49,17 @@ def create_bar_plot(year_start, year_end):
     chart = alt.Chart(df).mark_bar().encode(
         x=alt.X('tsunami_intensity:Q', title = 'Tsunami Intensity', scale=alt.Scale(domain=(0, 12))),
         y=alt.Y('tsunami_instance:N', sort = '-x', title = 'Country', axis = alt.Axis(labelExpr="datum.country")),
-        color=alt.Color('country:O', legend=alt.Legend(title="Countries (up to Top 10)")),
+        color=alt.Color('country:O', legend=alt.Legend(title="Countries (Top 10)")),
         tooltip=("country:O", "location_name:O", "tsunami_intensity:Q", "earthquake_magnitude:Q", "year:Q", "month:O")
-        ).properties(width=250, height=180).configure_legend(
-        titleFontSize=10,
-        labelFontSize=9
-    ) 
+        ).properties(width=250, height=180)
     
+    chart.configure_legend(padding=10,
+                            cornerRadius=10,
+                            orient='top-right')
+
     text = chart.mark_text(align="left", baseline="middle", dx = 3).encode(
         text= 'combine:O')
     
     plot = chart + text
+    plot.properties
     return plot.to_html()
